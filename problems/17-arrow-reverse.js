044d749773c0eb19aafb73eb781484ab0af763de
Kendra <cruickshank.kendra@gmail.com>
2024-04-04 16:49:00 -0700

    Q17

--- problems/17-arrow-reverse.js
+++ problems/17-arrow-reverse.js
@@ -12,7 +12,13 @@ let result3 = arrowReverseString("potato"); // returns "otatop"
 
 ***********************************************************************/
 
-// Your code here 
+const arrowReverseString = (str) => {
+  let answer = "";
+  for (let i = str.length-1; i>= 0; i--) {
+    answer += str[i];
+  }
+  return answer;
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 

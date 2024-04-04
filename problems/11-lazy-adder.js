a464274f59bd0a9604ea78302f03c7f086c4d839
Kendra <cruickshank.kendra@gmail.com>
2024-04-04 15:51:00 -0700

    Q11

--- problems/11-lazy-adder.js
+++ problems/11-lazy-adder.js
@@ -22,7 +22,16 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
   fat-arrow function
 ***********************************************************************/
 
-// Your code here 
+const lazyAdder = (firstNum) => {
+  let sum = 0;
+  return function (secondNum) {
+    sum = firstNum + secondNum;
+    return function (thirdNum) {
+      sum += thirdNum;
+      return sum;
+    }
+  }
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 

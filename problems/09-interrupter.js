ac8e716549651429af47575f353f7d890472f364
Kendra <cruickshank.kendra@gmail.com>
2024-04-04 15:29:06 -0700

    Q9

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -16,7 +16,19 @@ console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 ***********************************************************************/
 
-// Your code here 
+const interrupter = (interruptingWord) => {
+  return function (string) {
+    let stringArr = string.split(" ");
+    let answer = [];
+    for (let word of stringArr) {
+      answer.push(word);
+      answer.push(interruptingWord);
+      
+    }
+    answer.pop();
+    return answer.join(" ");
+  }
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
